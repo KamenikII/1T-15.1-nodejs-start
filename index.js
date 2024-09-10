a4ce879f4499f2fd6bb4e20270b5ad552dbc75a2
Ilya <107436210+KamenikII@users.noreply.github.com>
2024-09-10 10:01:30 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// 1 ex
+// let a = 5
+// let b = 7
+// console.log(a+b)
+
+
+// 2 ex
+// let c = [2, 5, 8, 2, 5, 7, 2]
+// const { sort } = require('fast-sort')
+// c = sort(c).asc()
+// console.log(c)
+// c = sort(c).desc()
+// console.log(c)
+
+
+// HW
+const m = 5
+const n = 200
+
+const { getPrimes} = require("@sigma-js/primes");
+console.log( getPrimes(m, n) )
